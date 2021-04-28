e59f2f4529c9036ff0c8c5d76f39b451bfd65dc1
Luc Burns <burnsluc2@gmail.com>
2021-04-28 11:45:33 -0400

    Testing routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,23 @@
-const router = require('express').Router();
-const { Post, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', (req, res) => {
-})
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User, attributes: ["name"] }],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', {
+        posts,
+        logged_in: req.session.logged_in
+    })
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
