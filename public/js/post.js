f4522752f85a10b162b0181da8cda2887ac0d0ec
Luc Burns <burnsluc2@gmail.com>
2021-04-28 15:42:27 -0400

    js functionality for post page

---
+++ public/js/post.js
@@ -0,0 +1,26 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+
+    const content = document.querySelector('#post-content').value.trim();
+    const id = document.querySelector('#post_id').value;
+
+    if (content) {
+        const response = await fetch("/api/comments", {
+            method: "POST",
+            body: JSON.stringify({ content, post_id: id }),
+            headers: { "Content-Type": "application/json" },
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    } else {
+        alert("You must enter a comment to comment");
+    }
+}
+
+document
+.querySelector('.new-comment-form')
+.addEventListener('submit', commentFormHandler);
