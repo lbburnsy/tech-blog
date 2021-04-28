5cbdcf1ec4dbf768d3d7f65127441ec04a91e23f
Luc Burns <burnsluc2@gmail.com>
2021-04-28 10:25:11 -0400

    add comment to object

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
-    console.log("homeRoutes working");
 })
 
 module.exports = router;
