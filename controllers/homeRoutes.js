a90ab4f46a74ee9231a71f4921c223b8cae27b18
Luc Burns <burnsluc2@gmail.com>
2021-04-29 11:20:53 -0400

    logout bug

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,6 +19,23 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/logout", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User, attributes: ["name"] }],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get("/post/:id", withAuth, async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
